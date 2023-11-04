8f26d7e193d643162aad1a663536f9b49b0461f7
lauravramirez27 <lauravramirez0327@gmail.com>
2023-11-03 19:24:03 -0500

    feat: :sparkles: add database

---
+++ config/conection.js
@@ -0,0 +1,19 @@
+import {MongoClient} from "mongodb";
+import dotenv from "dotenv";
+dotenv.config();
+
+export async function con(){
+    try{
+       /*  const uri= `mongodb+srv://root:${process.env.ATLAS_PASSWORD}@cluster0.l3dtumk.mongodb.net/${process.env.ATLAS_DB}` */
+       const uri =`mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASSWORD}@prueba.52kawul.mongodb.net/${process.env.ATLAS_DB}`
+              
+        const options ={
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        };
+        const client = await MongoClient.connect(uri, options);
+        return client.db();
+    }catch(error){
+        return { status:500, message: error}
+    }
+}
